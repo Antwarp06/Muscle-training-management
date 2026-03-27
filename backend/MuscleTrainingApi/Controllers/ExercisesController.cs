73a2381512409f7f15fa87f9b668906d37ca378d
Antwarp06 <yasuda.naoki.xr@gmail.com>
2026-03-27 15:58:40 +0900

    部位・種目の削除機能(backend)

---
+++ backend/MuscleTrainingApi/Controllers/ExercisesController.cs
@@ -0,0 +1,33 @@
+using Microsoft.AspNetCore.Mvc;
+using Npgsql;
+
+namespace MuscleTrainingApi.Controllers;
+
+[Route("api/[controller]")]
+[ApiController]
+public class ExercisesController : ControllerBase{
+    private readonly string _connectionString;
+
+    public ExercisesController(IConfigration configuration){
+        _connectionString = configuration.GetConnectionString("DefaultConnection")?? "";
+    }
+
+    [HttpDelete({"id"})]
+    public async Task<IActionResult> Delte(int id){
+        using var conn = new NpgsqlConnection(_connectionString);
+        await conn.OpenAsync();
+
+        try{
+            using var cmd = new NpgsqlCommand("DELETE FROM \"Exercises\" WHERE \"Exercise_Id\" = @id", conn);
+            cmd.Parameters.AddWithValue("id",id);
+
+            int affectedRows = await cmd.ExecuteNonQueryAsync();
+            if(affectedRows == 0) return NotFound();
+
+            return Ok(new {message = "種目を削除しました"});
+        }
+        catch (PostgreException ex) when (ex.SqlState == "23503"){
+            return BadRequest(new {message = "この種目のトレーニング記録が存在するため削除できません。"});
+        }
+    }
+}
